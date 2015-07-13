31cdf1fc3c2d8beef03a5c69a9eb230ae129c5db
Jay Pujara <github@puuj.org>
2015-07-13 16:09:27 +0200

    Some fixes to run script

--- experiments/jester/run.sh
+++ experiments/jester/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 bash ./fetchDataset.sh
 bash ./fetchDependencies.sh
+mvn install:install-file -Dfile=./jars/psl-groovy-1.3.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-groovy -Dversion=1.3.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=./jars/psl-core-1.3.1-SNAPSHOT.jar -DgroupId=edu.umd.cs -DartifactId=psl-core -Dversion=1.3.1-SNAPSHOT -Dpackaging=jar
+cd model
 mvn clean compile
-java -Xmx4G -cp ./jars/psl-core-1.3.1-SNAPSHOT.jar:./jars/psl-groovy-1.3.1-SNAPSHOT.jar:./jars/psl-archetype-groovy-1.3.1-SNAPSHOT.jar:./target/classes edu.umd.cs.onlineinfer.jester.Jester quad 100 10
-java -Xmx4G -cp ./jars/psl-core-1.3.1-SNAPSHOT.jar:./jars/psl-groovy-1.3.1-SNAPSHOT.jar:./jars/psl-archetype-groovy-1.3.1-SNAPSHOT.jar:./target/classes edu.umd.cs.onlineinfer.jester.JesterApproximate quad 100 10 wtlagrangemax
+java -Xmx4G -cp ../jars/psl-core-1.3.1-SNAPSHOT.jar:../jars/psl-groovy-1.3.1-SNAPSHOT.jar:../jars/psl-archetype-groovy-1.3.1-SNAPSHOT.jar:./target/classes edu.umd.cs.onlineinfer.jester.Jester quad 100 10
+java -Xmx4G -cp ../jars/psl-core-1.3.1-SNAPSHOT.jar:../jars/psl-groovy-1.3.1-SNAPSHOT.jar:../jars/psl-archetype-groovy-1.3.1-SNAPSHOT.jar:./target/classes edu.umd.cs.onlineinfer.jester.JesterApproximate quad 100 10 wtlagrangemax
 
